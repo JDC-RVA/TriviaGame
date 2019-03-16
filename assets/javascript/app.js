d7aa496a68bac101921a31da269db696c77b7a32
JDC-RVA <jeremy@firstmotionlc.com>
2019-03-16 00:34:39 -0400

    added html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,10 @@
 
+//   Start timer once the page loads
   window.onload = function(){
     $("#start").on("click", start);
 
     (function start(){
-      var counter = 60;
+      var counter = 24;
     
       setInterval(function() {
         counter--;
@@ -12,7 +13,8 @@
           span.innerHTML = counter;
         }
         if (counter === 0) {
-            clearInterval(counter);
+            // clearInterval(counter);
+            alert("SHOT CLCOK VIOLATION!")
         }
     
       }, 1000);
@@ -21,25 +23,35 @@
     
     }
 
-    // Questions
+    // Array of questions that could be used to dynamically generate question in the DOM
     var triviaQuestions = [{
         question: "Who is the shortest player of all-time?",
-        answerList: ["Allen Iverson", "Tyrone 'Muggsy' Bogues", "Isaiah Thomas", "John Stockton"],
+        answerList: ["Allen Iverson", "Tyrone 'Muggsy' Bogues", "Isaiah Thomas"],
         answer: 1
     },{
         question: "What team drafted Kobe Bryant?",
-        answerList: ["Lakers", "Suns", "Hornets", "Wizards"],
+        answerList: ["Lakers", "Suns", "Hornets"],
         answer: 2
     },{
         question: "What player has the highest career FT percentage?",
-        answerList: ["Steve Nash", "John Stockton", "Stephen Curry", "Ray Allen"],
+        answerList: ["Steve Nash", "John Stockton", "Stephen Curry"],
         answer: 0
     },{
         question: "Who has the most coaching wins?",
-        answerList: ["Greg Popovich", "Steve Kerr", "Don Nelson", "Phil Jackson"],
+        answerList: ["Greg Popovich", "Steve Kerr", "Don Nelson"],
         answer: 2
     },{
         question: "What player scored the most points in one game?",
-        answerList: ["Kobe Bryant", "Lebron James", "Michael Jordan", "Wilt Chamberlain"],
-        answer: 3
+        answerList: ["Kobe Bryant", "Wilt Chamberlain", "Michael Jordan"],
+        answer: 1
     }];
+
+    // create variables to contain the correct answers
+
+    // create a function to record user input
+
+    // create a function to check user input against correct answers
+
+    // Variables to record answers
+    var correctGuesses = 0;
+    var incorrectGuesses = 0;
